3a942f71a6382a6a70adbeb44fae50b6334cda59
Shifa04 <shifarabiya4216@gmail.com>
2025-10-01 13:54:27 +0530

    feat: create dedicated program page component

---
+++ client/src/program.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './nav';
+import HowItWorks from './howItWorks';
+import Footer from './footer';
+
+function Program() {
+  return (
+    <div className="program" id="program">
+      <Nav />
+      <HowItWorks />
+      <Footer />
+    </div>
+  );
+}
+
+export default Program;
